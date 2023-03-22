1c37338408d8581a232788ea2a49d59d7a0ce8b7
Mounika1379 <123626361+Mounika1379@users.noreply.github.com>
2023-03-22 17:38:52 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "five" {
   name              = "mounika-terraform-elb"
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
+  availability_zones = ["ap-south-1a", "ap-south-1c"]
   listener {
     instance_port     = 80
     instance_protocol = "http"
