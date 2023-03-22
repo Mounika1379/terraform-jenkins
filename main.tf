f3544da9d60ac280fd67c2c4c8cc0ed686904400
Mounika1379 <123626361+Mounika1379@users.noreply.github.com>
2023-03-22 19:22:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_instance" "two" {
   instance_type         = "t2.micro"
   key_name              = "mounika"
   vpc_security_group_ids = [aws_security_group.three.id]
-  availability_zone     = "ap-south-1c"
+  availability_zone     = "ap-south-1b"
   user_data             = <<EOF
 #!/bin/bash
 sudo -i
