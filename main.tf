125cd5992b16aa50f5f21a6a6c5b4faa6c1491dc
Mounika1379 <123626361+Mounika1379@users.noreply.github.com>
2023-03-22 19:36:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "two" {
   key_name              = "mounika"
   vpc_security_group_ids = [aws_security_group.three.id]
   availability_zone     = "ap-south-1b"
-  user_data              + <<EOF
+  user_data              = <<EOF
 #!/bin/bash
 sudo -i
 yum install httpd -y
