dfdf0a8f826f075fce529a922dfb5d27bd0a0708
Mounika1379 <123626361+Mounika1379@users.noreply.github.com>
2023-03-22 18:50:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,7 @@ provider "aws" {
 region = "ap-south-1"
 access_key = "AKIA3OWTCUQACKQBTXYZ"
 secret_key = "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"
-  
 }
-
 resource "aws_instance" "one" {
   ami                   = "ami-005f9685cb30f234b"
   instance_type         = "t2.micro"
